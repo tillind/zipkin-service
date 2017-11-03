ef87d1922cb04003336548b6a8d4bf86a8031fa6
alexandre <gadeau.alexandre@laposte.net>
2017-11-03 17:44:23 +0100

    Initial Commit

---
+++ src/main/java/org/miage/m2/ZipkinApplication.java
@@ -0,0 +1,16 @@
+package org.miage.m2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import zipkin.server.EnableZipkinServer;
+
+@EnableZipkinServer
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ZipkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipkinApplication.class, args);
+	}
+}
